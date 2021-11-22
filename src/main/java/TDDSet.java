df2e99c6efe77043f714bbd1eb6a4dbe15e23dc1
Andrés Torres <towerspro@gmail.com>
2021-11-22 18:28:58 +0100

    refactor: add size field

--- src/main/java/TDDSet.java
+++ src/main/java/TDDSet.java
@@ -1,21 +1,24 @@
 public class TDDSet<T> {
 
     private Object[] elements;
+    private int size;
 
     public TDDSet() {
-        this.elements = new Object[]{};
+        this.elements = new Object[10];
+        this.size = 0;
     }
 
     public boolean isEmpty() {
-        return this.elements.length == 0;
+        return this.size == 0;
     }
 
     public boolean add(T element) {
-        this.elements = new Object[] { element };
+        this.elements[this.size] = element;
+        this.size++;
         return true;
     }
 
     public int size() {
-        return 2;
+        return size;
     }
 }
