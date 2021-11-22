c2e71866991f331044dca6efcef875934e5342b8
Andrés Torres <towerspro@gmail.com>
2021-11-22 18:06:40 +0100

    green: isEmpty and add pass

--- src/main/java/TDDSet.java
+++ src/main/java/TDDSet.java
@@ -1,10 +1,17 @@
 public class TDDSet<T> {
 
+    private Object[] elements;
+
+    public TDDSet() {
+        this.elements = new Object[]{};
+    }
+
     public boolean isEmpty() {
-        return true;
+        return this.elements.length == 0;
     }
 
     public boolean add(T element) {
-        return false;
+        this.elements = new Object[] { element };
+        return true;
     }
 }
